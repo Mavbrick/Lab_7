3837839218178676dd9be08e504c784399afdd56
Mavbrick <mbaut030@ucr.edu>
2021-04-30 18:30:29 -0700

    Initializing repository n Completed part 1

---
+++ source/main.c
@@ -0,0 +1,40 @@
+/* Author: Maverick Bautista
+ * Lab Section: 023
+ * Assignment: Lab 7  Exercise 1
+ * Exercise Description: [optional - include for your own benefit]
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code is my own original work.
+ *
+ *  Demo Link: https://youtu.be/kZNUyT1OLbo 
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char tmpB = 0x00;
+	unsigned char tmpD = 0x00;
+	ADC_init();
+    /* Insert your solution below */
+    while (1) {
+	unsigned short x = ADC;
+	tmpB = (char)x;
+	tmpD = (char)(x >> 8);
+
+	PORTB = tmpB;
+	PORTD = tmpD;
+    }
+    return 1;
+}
